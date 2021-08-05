29d13cdaf5be8ae5aacdcccebd77e6a2a5dae1ab
TsikaBullRat <tsika1996190514@gmail.com>
2021-08-05 17:05:25 +0200

    New Changes

--- src/comp/Questions.js
+++ src/comp/Questions.js
@@ -4,13 +4,25 @@ export const Questions = props => {
 
     const style = {
         button: {
-            height: '10%',
-            width: '50%',
-            fontSize: '20px',
-            marginLeft: '60%',
-            background: '#3cbc8d',
-            transition: '0.5s',
-            textAlign: 'center'
+            On: {
+                height: '10%',
+                width: '50%',
+                fontSize: '20px',
+                marginLeft: '60%',
+                background: '#3cbc8d',
+                transition: '0.5s',
+                textAlign: 'center'
+            },
+            Off:{
+                height: '10%',
+                width: '50%',
+                fontSize: '20px',
+                marginLeft: '60%',
+                background: '#3cbc8d',
+                transition: '0.5s',
+                textAlign: 'center'
+            }
+
         },
         box: {
             marginTop: '2%',
@@ -25,52 +37,52 @@ export const Questions = props => {
             id: "0",
             question: "Do you have a fever?",
             answer: false,
-            style: style.button,
+            style: style.button.Off,
             text: "No",
         },
         {
             id: "1",
             question: "Do you have a dry cough?",
             answer: false,
-            style: style.button,
+            style: style.button.Off,
             text: "No",
         },
         {
             id: "2",
             question: "Do you have a running nose?",
             answer: false,
-            style: style.button,
+            style: style.button.Off,
             text: "No",
         },
         {
             id: "3",
             question: "Do you have a soar throat or diarhea?",
             answer: false,
-            style: style.button,
+            style: style.button.Off,
             text: "No",
         },
         {
             id: "4",
             question: "Have you been out of the pronvice?",
             answer: false,
-            style: style.button,
+            style: style.button.Off,
             text: "No",
         },
         {
             id: "5",
             question: "Have you been in contact with someone who tested posivite for covid?",
             answer: false,
-            style: style.button,
+            style: style.button.Off,
             text: "No",
         }
     ])
     const setBool = val => {
-        var rem = val%2
+        var rem = val % 2
         console.log(val)
         console.log(rem)
-        if(rem = 0) {return true}
-        if(rem != 0) {return false}
-        
+        if (rem = 0) { return true }
+        if (rem != 0) { return false }
+
 
     }
 
@@ -78,44 +90,28 @@ export const Questions = props => {
 
 
         console.log(qna)
-        if(e.target.value = true){
+        if (e.target.value = true) {
             setQnA(qna.map((item, index) => {
                 if (index === parseInt(e.target.id)) {
                     item = {
                         id: e.target.id,
                         question: item.question,
                         answer: !item.answer,
-                        style: {
-                            height: '10%',
-                            width: '60%',
-                            fontSize: '20px',
-                            marginLeft: '0%',
-                            background: '#3cbc8d',
-                            transition: '0.5s',
-                            textAlign: 'center'
-                        },
+                        style: style.button.On,
                         text: "Yes",
                         clicker: parseInt(e.target.dataset.count) + 1
                     }
                     return item
                 } else return item
             }))
-        }else{//if(e.target.value = false)
+        } else {//if(e.target.value = false)
             setQnA(qna.map((item, index) => {
                 if (index === parseInt(e.target.id)) {
                     item = {
                         id: e.target.id,
                         question: item.question,
                         answer: !item.answer,
-                        style: {
-                            height: '10%',
-                            width: '60%',
-                            fontSize: '20px',
-                            marginLeft: '60%',
-                            background: '#3cbc8d',
-                            transition: '0.5s',
-                            textAlign: 'center'
-                        },
+                        style: style.button.Off,
                         text: "Yes",
                         clicker: parseInt(e.target.dataset.count) + 1
                     }
@@ -131,7 +127,7 @@ export const Questions = props => {
                 {qna.map(item => {
                     return (<div className="qna">
                         <h2>{item.question}</h2>
-                        <input id={item.id} type="checkbox" onClick={Change} value={item.answer}/>
+                        <input id={item.id} type="checkbox" onClick={Change} value={item.answer} />
                         <div style={style.box}>
                             <label htmlFor={item.id} style={item.style}>{item.text}</label>
                         </div>
